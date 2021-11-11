1de3fcbc15e05ed114a2bf47b381094285a9b835
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-11-11 16:43:11 -0300

    feat: form body in progress

---
+++ src/Components/forms/formCheckout/index.js
@@ -0,0 +1,15 @@
+export const FormCheckout = () => {
+  return (
+    <form>
+      <input placeholder="Nome" />
+      <input placeholder="Email" />
+      <input placeholder="Telefone" />
+      <input placeholder="Cep" />
+      <input placeholder="Endereço" />
+      <input placeholder="Bairro" />
+      <input placeholder="Número" />
+      <input placeholder="Complemento" />
+      <button type="submit">Tudo Pronto</button>
+    </form>
+  );
+};
