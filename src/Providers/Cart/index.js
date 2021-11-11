70f69de0471dd959d25371d3eb21922f72dac96f
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-11-11 16:05:14 -0300

    feat: provider cart implemented

---
+++ src/Providers/Cart/index.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState } from "react";
+
+export const CartContext = createContext({});
+
+export const CartProvider = ({ children }) => {
+  const [cartList, setCartList] = useState([]);
+  const [showCart, setShowCart] = useState(false);
+
+  const addToCart = (product) => {
+    setCartList([...cartList]);
+  };
+
+  const rmvToCart = (idProduct) => {
+    //refatorar depois
+    const newCartList = cartList
+      .filter((item) => item.id === idProduct)
+      .slice(1);
+    const filtred = cartList.filter((item) => item.id !== idProduct);
+    setCartList([...filtred, ...newCartList]);
+  };
+
+  const rmvAllIDProducts = (idProduct) => {
+    const newCartList = cartList.filter((item) => item.id !== idProduct);
+    setCartList(newCartList);
+  };
+
+  const clearCart = () => {
+    setCartList([]);
+  };
+
+  const handleShowCart = () => {
+    setShowCart(!showCart);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        handleShowCart,
+        rmvAllIDProducts,
+        rmvToCart,
+        addToCart,
+        clearCart,
+        cartList,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
