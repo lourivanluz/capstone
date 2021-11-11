62aa15eb130b0c808401841f49edd7887f75d097
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-11-11 16:15:41 -0300

    feat: add providers for aplication

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ResetCss } from "./Global/Style/Reset";
+import { Providers } from "./Providers";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ResetCss />
-      <App />
+      <Providers>
+        <ResetCss />
+        <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
