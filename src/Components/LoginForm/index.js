a4eea9bd85e78493e04a9e619044c84c33aed940
Arthur Sarmento <arthurbs17@gmail.com>
2021-11-12 12:17:01 -0300

    feat: component login done

---
+++ src/Components/LoginForm/index.js
@@ -0,0 +1,29 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import schema from "./schema";
+import { useUser } from "../../Providers/User";
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const { loginAccount } = useUser();
+
+  const handleLogin = (data) => {
+    loginAccount(data);
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleLogin)}>
+      <input placeholder="Email" {...register("email")} />
+      <input placeholder="Senha" {...register("password")} />
+      <button>Entrar</button>
+    </form>
+  );
+};
+
+export default LoginForm;
