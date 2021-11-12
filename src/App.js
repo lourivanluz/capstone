9c3c757e44fe988b71b1c11ac83a82f8c77951f3
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-11-12 16:53:23 -0300

    feat: cart funcionando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { Cart } from "./Components/Cart";
-import { useCart } from "./Providers/Cart";
 import { Routers } from "./Routes";
 
 function App() {
-  const { handleShowCart, showCart } = useCart();
   return (
     <div>
-      <button onClick={handleShowCart}>cart</button>
       <Routers />
-      <Cart show={showCart} />
     </div>
   );
 }
