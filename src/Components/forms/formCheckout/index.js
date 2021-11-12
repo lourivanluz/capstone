16480101c4b96256ab74e3a31437d2248f5f0847
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-11-12 12:43:27 -0300

    feat: formulario incompleto

--- src/Components/forms/formCheckout/index.js
+++ src/Components/forms/formCheckout/index.js
@@ -22,39 +22,41 @@ export const FormCheckout = () => {
     resolver: yupResolver(formSchema),
   });
 
-  const handleData = (data) => {
-    console.log(data);
+  const handleData = (_) => {
+    console.log("mudei o showCart pra true");
   };
 
   return (
-    <form onSubmit={handleSubmit(handleData)}>
-      <input placeholder="Nome" type="text" {...register("name")} />
-      {errors && <p>{errors.name?.message}</p>}
+    <div>
+      <form onSubmit={handleSubmit(handleData)}>
+        <input placeholder="Nome" type="text" {...register("name")} />
+        {errors && <p>{errors.name?.message}</p>}
 
-      <input placeholder="Email" type="email" {...register("email")} />
-      {errors && <p>{errors.email?.message}</p>}
+        <input placeholder="Email" type="email" {...register("email")} />
+        {errors && <p>{errors.email?.message}</p>}
 
-      <input placeholder="Telefone" type="number" {...register("phone")} />
-      {errors && <p>{errors.phone?.message}</p>}
+        <input placeholder="Telefone" type="number" {...register("phone")} />
+        {errors && <p>{errors.phone?.message}</p>}
 
-      <input placeholder="Cep" type="number" {...register("cep")} />
-      {errors && <p>{errors.cep?.message}</p>}
+        <input placeholder="Cep" type="number" {...register("cep")} />
+        {errors && <p>{errors.cep?.message}</p>}
 
-      <input placeholder="Endereço" type="text" {...register("address")} />
-      {errors && <p>{errors.adress?.message}</p>}
+        <input placeholder="Endereço" type="text" {...register("address")} />
+        {errors && <p>{errors.adress?.message}</p>}
 
-      <input placeholder="Bairro" type="text" {...register("area")} />
-      {errors && <p>{errors.area?.message}</p>}
+        <input placeholder="Bairro" type="text" {...register("area")} />
+        {errors && <p>{errors.area?.message}</p>}
 
-      <input placeholder="Número" type="text" {...register("number")} />
-      {errors && <p>{errors.number?.message}</p>}
-      <input
-        placeholder="Complemento"
-        type="text"
-        {...register("complement")}
-      />
-      {errors && <p>{errors.complement?.message}</p>}
-      <input type="submit" />
-    </form>
+        <input placeholder="Número" type="text" {...register("number")} />
+        {errors && <p>{errors.number?.message}</p>}
+        <input
+          placeholder="Complemento"
+          type="text"
+          {...register("complement")}
+        />
+        {errors && <p>{errors.complement?.message}</p>}
+        <button type="submit"> Tudo Pronto </button>
+      </form>
+    </div>
   );
 };
