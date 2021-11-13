beb2c1990f5ccb4a33d4d33de68a8e1c2122f7f0
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-11-13 17:30:15 -0300

    feat: feito a logica de adicionar o cart no localStorage

--- src/Providers/Cart/index.js
+++ src/Providers/Cart/index.js
@@ -3,11 +3,14 @@ import { createContext, useContext, useState } from "react";
 export const CartContext = createContext({});
 
 export const CartProvider = ({ children }) => {
-  const [cartList, setCartList] = useState([]);
+  const [cartList, setCartList] = useState(() => {
+    return JSON.parse(localStorage.getItem("@BHealthy: cart")) || [];
+  });
   const [showCart, setShowCart] = useState(false);
 
   const addToCart = (product) => {
     setCartList([...cartList, product]);
+    localStorage.setItem("@BHealthy: cart", JSON.stringify(cartList));
   };
 
   const rmvToCart = (idProduct) => {
@@ -17,15 +20,18 @@ export const CartProvider = ({ children }) => {
       .slice(1);
     const filtred = cartList.filter((item) => item.id !== idProduct);
     setCartList([...filtred, ...newCartList]);
+    localStorage.setItem("@BHealthy: cart", JSON.stringify(cartList));
   };
 
   const rmvAllIDProducts = (idProduct) => {
     const newCartList = cartList.filter((item) => item.id !== idProduct);
     setCartList(newCartList);
+    localStorage.setItem("@BHealthy: cart", JSON.stringify(cartList));
   };
 
   const clearCart = () => {
     setCartList([]);
+    localStorage.setItem("@BHealthy: cart", JSON.stringify(cartList));
   };
 
   return (
