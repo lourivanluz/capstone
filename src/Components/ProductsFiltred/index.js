560cc6beeacf9affc4a9f6918a2ae19cf8027d6b
Arthur Sarmento <arthurbs17@gmail.com>
2021-11-15 14:00:31 -0300

    refatored: Funçao no component de categoria refatorado

--- src/Components/ProductsFiltred/index.js
+++ src/Components/ProductsFiltred/index.js
@@ -1,18 +1,20 @@
+import { useEffect } from "react";
 import { useParams } from "react-router";
 import { useProducts } from "../../Providers/Products";
 import CardProduct from "../CardProduct";
 
 export const ProductFiltred = () => {
   const { category } = useParams();
-  const { filterPerCategory, products } = useProducts();
-  //const listCategory = filterPerCategory(`${category}`);
-  const listCategory = products.filter((item) => item.category === category);
-  console.log(listCategory);
+  const { filterPerCategory, filteredProducts } = useProducts();
+
+  useEffect(() => {
+    filterPerCategory(category);
+  }, [category]);
 
   return (
     <div>
       <ul>
-        {listCategory.map((item, index) => (
+        {filteredProducts.map((item, index) => (
           <li key={index}>
             <CardProduct product={item} />
           </li>
