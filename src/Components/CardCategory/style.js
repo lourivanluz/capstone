761cf48751b678a684b01e8d3999f2599cbfa759
Arthur Sarmento <arthurbs17@gmail.com>
2021-11-16 10:44:09 -0300

    refactored: tamanho dos cards e do carrossel de produtos refeitos

--- src/Components/CardCategory/style.js
+++ src/Components/CardCategory/style.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const CardCategoryContainer = styled.li`
-  width: 200px;
-  min-width: 200px;
-  height: 300px;
+  width: 150px;
+  min-width: 150px;
+  height: 260px;
   border: 1px solid #000;
   transition: transform 0.75s;
   background-image: url(${(props) => props.imageBackground});
-  background-size: 200px 300px;
+  background-size: 149px 259px;
   :hover {
     transform: translate(-1px, -1px);
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.5);
@@ -15,10 +15,10 @@ export const CardCategoryContainer = styled.li`
 `;
 
 export const CardCategoryContent = styled.div`
-  width: 200px;
-  min-width: 200px;
-  height: 300px;
-  background-color: rgba(253, 245, 230, 0.5);
+  width: 149px;
+  min-width: 149px;
+  height: 259px;
+  background-color: rgba(253, 245, 230, 0.4);
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -26,6 +26,8 @@ export const CardCategoryContent = styled.div`
   h1 {
     font-weight: bold;
     font-variant: small-caps;
+    text-align: center;
+    font-size: 20px;
     color: #000000;
   }
 `;
--- src/Components/CardProduct/style.js
+++ src/Components/CardProduct/style.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 
 export const ProductCardContainer = styled.li`
-  width: 200px;
-  min-width: 200px;
-  height: 300px;
+  width: 150px;
+  min-width: 150px;
+  height: 260px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  padding: 10px;
   border: 1px solid #000;
   transition: transform 0.75s;
   img {
-    width: 180px;
-    height: 120px;
+    width: 130px;
+    height: 90px;
+    border-radius: 5px;
     cursor: pointer;
   }
   div {
@@ -22,10 +24,15 @@ export const ProductCardContainer = styled.li`
     justify-content: space-between;
   }
   p {
-    padding: 4px;
-    font-size: 16px;
+    font-size: 12px;
     font-weight: bold;
     cursor: pointer;
+  }
+  span {
+    font-size: 12px;
+  }
+  button {
+    margin-top: 10px;
   }
   :hover {
     transform: translate(-1px, -1px);
