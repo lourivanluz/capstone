921d34c4b22ec71109072938cf99c50c6aa33666
Gabriel Azevedo <gmarquesazti@gmail.com>
2021-11-17 15:25:52 -0300

    style: estilização funcional do footer finalizada

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,42 +5,44 @@ import { Link } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Container>
+    <>
       <FooterForm />
-      <AboutContent>
-        <div>
-          <h1>Sobre</h1>
-          <p>
-            Esse site é um projeto de estudos de alunos da Kenzie Academy Brasil
-            com o objetivo de colocar nossos conhecimentos técnicos e
-            habilidades de comunicação em prática.
-          </p>
-        </div>
-        <div>
-          <h1>Integrantes</h1>
-          <ul>
-            <li>
-              Arthur Sarmento{" "}
-              <Link href="https://github.com/arthurbs17">
-                <FaGithub />
-              </Link>
-            </li>
-            <li>
-              Gabriel Azevedo{" "}
-              <Link href="https://github.com/azgabe">
-                <FaGithub />
-              </Link>
-            </li>
-            <li>
-              Lourivan Rodrigues{" "}
-              <Link href="https://github.com/lourivanluz">
-                <FaGithub />
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </AboutContent>
-    </Container>
+      <Container>
+        <AboutContent>
+          <div>
+            <h3>Sobre</h3>
+            <p>
+              Esse site é um projeto de estudos de alunos da Kenzie Academy
+              Brasil com o objetivo de colocar nossos conhecimentos técnicos e
+              habilidades de comunicação em prática.
+            </p>
+          </div>
+          <div>
+            <h3>Integrantes</h3>
+            <ul>
+              <li>
+                Arthur Sarmento{" "}
+                <Link href="https://github.com/arthurbs17">
+                  <FaGithub />
+                </Link>
+              </li>
+              <li>
+                Gabriel Azevedo{" "}
+                <Link href="https://github.com/azgabe">
+                  <FaGithub />
+                </Link>
+              </li>
+              <li>
+                Lourivan Rodrigues{" "}
+                <Link href="https://github.com/lourivanluz">
+                  <FaGithub />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </AboutContent>
+      </Container>
+    </>
   );
 };
 
--- src/Components/FooterForm/style.js
+++ src/Components/FooterForm/style.js
@@ -2,12 +2,11 @@ import styled from "styled-components";
 
 export const FormBackground = styled.form`
   width: 100%;
-  border-top: 48px solid var(--color-primary);
+  border-top: 96px solid var(--color-primary);
 `;
 
 export const FormContent = styled.div`
   display: flex;
-  min-height: 372px;
   width: 556px;
   max-width: 80vw;
   flex-flow: column nowrap;
@@ -17,24 +16,38 @@ export const FormContent = styled.div`
   h2 {
     font-size: 2em;
     text-align: center;
-    margin-bottom: 1.5rem;
+    margin: 1.5rem 0;
   }
 
   .flex {
     display: flex;
+    flex-direction: column;
+
     .flex-left {
-      margin-right: 0.5rem;
       margin-bottom: 1rem;
-      width: 50%;
+      width: 100%;
     }
     .flex-right {
-      margin-left: 0.5rem;
       margin-bottom: 1rem;
-      width: 50%;
+      width: 100%;
+    }
+
+    @media (min-width: 640px) {
+      flex-direction: row;
+
+      .flex-left {
+        margin-right: 0.5rem;
+        width: 50%;
+      }
+      .flex-right {
+        margin-left: 0.5rem;
+        width: 50%;
+      }
     }
   }
 
   button {
     margin-top: 1rem;
+    margin-bottom: 1.5rem;
   }
 `;
