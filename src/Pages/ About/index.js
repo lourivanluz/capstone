0a7cc710cbdf443c6a5ed3a74b426c78d0b0e5d9
Gabriel Azevedo <gmarquesazti@gmail.com>
2021-11-18 00:05:46 -0300

    style: estilização base página de assinatura

--- src/Pages/ About/index.js
+++ src/Pages/ About/index.js
@@ -1,5 +1,17 @@
 import AboutInfo from "../../Components/AboutnInfo";
+import CarouselBoxes from "../../Components/CarouselBoxes";
+import { useBox } from "../../Providers/Box";
+import { PageBase } from "../../Components/PageBase";
+import DetailsAccordion from "../../Components/DetailsAccordion";
 
 export const About = () => {
-  return <AboutInfo />;
+  const { boxList } = useBox();
+
+  return (
+    <PageBase>
+      <AboutInfo />
+      <CarouselBoxes products={boxList} />
+      <DetailsAccordion />
+    </PageBase>
+  );
 };
